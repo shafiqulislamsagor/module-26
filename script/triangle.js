344e74e7fae8add0a5d541fab70630140a0aca16
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2024-02-13 01:06:43 +0600

    some

--- script/triangle.js
+++ script/triangle.js
@@ -2,10 +2,17 @@ function triangleArea(){
     const baseArea = document.getElementById('base');
     const baseText = baseArea.value;
     const base = parseFloat(baseText);
-    console.log(base)
+    // console.log(base)
 
     const heightArea = document.getElementById('height');
     const heightText = heightArea.value;
     const height = parseFloat(heightText);
-    console.log(height)
+    // console.log(height)
+    const area = (1/2) * base * height;
+    // console.log(area)
+    const triangleAreaSpan = document.getElementById('areaName');
+    triangleAreaSpan.innerText = area;
+
+    
 }
+document.getElementById('div').style.textAlign = 'left';
