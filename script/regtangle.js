8fde8f1a520c2d1af1cf85ba5f72fef0602a48a3
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2024-02-13 03:24:35 +0600

    somthing

--- script/regtangle.js
+++ script/regtangle.js
@@ -1,17 +1,47 @@
 const buttons = document.getElementById('RectangleName');
 
-buttons.addEventListener('click',function(){
-    const weidthInputArea = document.getElementById('weidth').value;
-    const weidth = parseFloat(weidthInputArea);
+// buttons.addEventListener('click',function(){
+//     const weidthInputArea = document.getElementById('weidth').value;
+//     const weidth = parseFloat(weidthInputArea);
 
-    const lengthInputArea = document.getElementById('length').value;
+//     const lengthInputArea = document.getElementById('length').value;
+//     const length = parseFloat(lengthInputArea);
+
+//     const area = weidth * length;
+
+//     document.getElementById('rectangleArea').innerText = area
+
+// })
+
+function similar(man) {
+    const lengthInputArea = document.getElementById(man).value;
     const length = parseFloat(lengthInputArea);
+    return length;
+}
 
-    const area = weidth * length;
+buttons.addEventListener('click', function (a, b) {
+    a = similar('weidth');
+    b = similar('length')
+
+    const area = a * b;
 
     document.getElementById('rectangleArea').innerText = area
-    
+
 })
+
+
+function triangleArea(a, b) {
+    a = similar('base')
+    b = similar('height')
+    const areas = (1 / 2) * a * b;
+    // console.log(area)
+    const triangleAreaSpan = document.getElementById('areaName');
+    triangleAreaSpan.innerText = areas;
+
+
+}
+
+
 
 // function buttonsReg(){
 //     const weidthInputArea = document.getElementById('weidth').value;
--- script/triangle.js
+++ script/triangle.js
@@ -1,22 +1,4 @@
-function triangleArea(){
-    const baseArea = document.getElementById('base');
-    const baseText = baseArea.value;
-    const base = parseFloat(baseText);
-    // console.log(base)
 
-    const heightArea = document.getElementById('height');
-    const heightText = heightArea.value;
-    const height = parseFloat(heightText);
-    // console.log(height)
-    const areas = (1/2) * base * height;
-    // console.log(area)
-    const triangleAreaSpan = document.getElementById('areaName');
-    triangleAreaSpan.innerText = areas;
-
-    
-}
-
-const div = document.getElementsByClassName('div');
 
 for(let i of div){
     i.style.textAlign = 'left';
